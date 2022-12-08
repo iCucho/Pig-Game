73c98d650c8cb0ee46ed2a99a369a8523451b19b
iCucho <cuchoo24@hotmail.com>
2022-12-07 18:31:05 -0600

    Update PigsGame.js

--- PigsGame.js
+++ PigsGame.js
@@ -83,7 +83,7 @@ btnHold.addEventListener('click', function () {
     document.querySelector(`#score--${activePlayer}`).textContent =
       scores[activePlayer];
     // check if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 30) {
       // finish game
       isGameActive = false;
       document
